cb9fde6af88521802b06db19e3e2cf9a205e3e7e
Abhay Goyal <abhaygoyal0776@gmail.com>
2023-04-25 15:34:48 +0530

    Changes made

--- map.cjs
+++ map.cjs
@@ -2,7 +2,7 @@ let selfMap = (elements,cb) => {
     let ans = [];
     let k;
     for (let index=0; index<elements.length; index++){
-        k = cb(elements[index]);
+        k = cb(elements[index],index,elements);
         ans.push(k);
     }
     return ans;
--- reduce.cjs
+++ reduce.cjs
@@ -1,10 +1,10 @@
 let reduce = (elements, cb, startingValue) => {
     for (let index=0; index<elements.length; index++){
         if (startingValue === undefined) {
-            cb(elements[0],elements[index]);
+            cb(elements[0],elements[index],index,elements);
         }
         else {
-            cb(startingValue,elements[index]);
+            cb(startingValue,elements[index],index,elements);
         }
     }
 }
